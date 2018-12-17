4d05cdaca00e06d3c9af84614dec7b2141ba9674
Kai Chang <kaijchang@gmail.com>
2018-12-16 19:10:07 -0800

    check middle third only

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ border = [0, 20]
 
 ball = cv2.imread('templates/Ball.png', 0)
 
-threshold = 0.95
+# threshold = 0.95
 
 try:
     while True:
@@ -21,19 +21,20 @@ try:
             cv2.COLOR_BGR2GRAY
         )
 
-        top = img[0: int(img.shape[0] / 2), 0: img.shape[1]]
-        bottom = img[int(img.shape[0] / 2): img.shape[0], 0: img.shape[1]]
+        ball_area = img[int(img.shape[0] / 2): img.shape[0], int(img.shape[1] * 1 / 3): int(img.shape[1] * 2 / 3)]
 
-        res = cv2.matchTemplate(bottom, ball, cv2.TM_CCORR_NORMED)
+        res = cv2.matchTemplate(ball_area, ball, cv2.TM_CCORR_NORMED)
 
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
         w, h = ball.shape[::-1]
 
         top_left = max_loc
         bottom_right = (top_left[0] + w, top_left[1] + h)
-        cv2.rectangle(bottom, top_left, bottom_right, 0, 2)
+        cv2.rectangle(ball_area, top_left, bottom_right, 0, 2)
 
-        cv2.imshow('image', np.concatenate((top, bottom), axis=0))
+        img[int(img.shape[0] / 2): img.shape[0], int(img.shape[1] * 1 / 3): int(img.shape[1] * 2 / 3)] = ball_area
+
+        cv2.imshow('image', img)
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             raise KeyboardInterrupt
