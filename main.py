dd17a49d1a58a2e721f32b9effa949ebd355e8d9
Kai Chang <kaijchang@gmail.com>
2018-12-16 17:40:14 -0800

    only check bottom half of screen

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ game_res = [1152, 720]
 border = [0, 20]
 
 ball = cv2.imread('templates/Ball.png', 0)
-w, h = ball.shape[::-1]
+
+threshold = 0.95
 
 try:
     while True:
@@ -20,15 +21,19 @@ try:
             cv2.COLOR_BGR2GRAY
         )
 
-        res = cv2.matchTemplate(img, ball, cv2.TM_CCOEFF)
+        top = img[0: int(img.shape[0] / 2), 0: img.shape[1]]
+        bottom = img[int(img.shape[0] / 2): img.shape[0], 0: img.shape[1]]
+
+        res = cv2.matchTemplate(bottom, ball, cv2.TM_CCORR_NORMED)
+
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        w, h = ball.shape[::-1]
 
         top_left = max_loc
         bottom_right = (top_left[0] + w, top_left[1] + h)
+        cv2.rectangle(bottom, top_left, bottom_right, 0, 2)
 
-        cv2.rectangle(img, top_left, bottom_right, 255, 2)
-
-        cv2.imshow('image', img)
+        cv2.imshow('image', np.concatenate((top, bottom), axis=0))
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             raise KeyboardInterrupt
